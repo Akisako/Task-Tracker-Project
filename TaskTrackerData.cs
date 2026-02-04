5035c49e15b8d495f1a84983c322177840febf6d
Aki <46268842+Akisako@users.noreply.github.com>
2026-02-04 21:52:16 +0100

    Second Depression commit
    Updated EditJsonData method to append data to JSON file and added ReadJsonData method to read existing data.
    
    and it works kinda, still depressing commit

--- TaskTrackerData.cs
+++ TaskTrackerData.cs
@@ -10,14 +10,31 @@ namespace TaskTrackerJSON
         public bool InProgress {get; set;}
         public bool IsDone {get; set;}
 
-
         public static void EditJsonData(TaskTrackerData data)
         {
             string _fileName = "TaskTracker.json";
             string _jsonString = JsonSerializer.Serialize(data);
 
-            File.WriteAllText(_fileName, _jsonString);
+            if (data.ID == 1) // will overwrite the data
+                File.WriteAllText(_fileName, "["+_jsonString+"]"); 
+
+            var file_data = ReadJsonData();
+            if (file_data.ID != data.ID)
+                File.AppendAllText(_fileName, ",["+_jsonString+"]");
+                
             Console.WriteLine(File.ReadAllText(_fileName));
+        }
+
+        public static TaskTrackerData ReadJsonData()
+        {
+            string _fileName = "TaskTracker.json";
+            string _jsonString = File.ReadAllText(_fileName);
+            if (_jsonString.Length == 0)    
+                return new TaskTrackerData(); // breaks my code lmao
+
+            TaskTrackerData data = JsonSerializer.Deserialize<TaskTrackerData>(_jsonString)!;
+            
+            return data;
         }
 
         // TODO : 
